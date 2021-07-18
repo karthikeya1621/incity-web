4acf4bb80780fcb50341d8f33526eb645b968180
karthikeyay <karthikeya.yarlagadda@mobigesture.com>
2021-07-19 04:43:56 +0530

    Config

--- utils/config.ts
+++ utils/config.ts
@@ -2,8 +2,10 @@ export const NEXT_PUBLIC_GOOGLE_MAPS_API =
   "AIzaSyDt4Bnz3n7SErEtHwRso5prUZKIxIYPvos";
 
 export const URLS = {
-  partner_url: "http://localhost:4000/",
-  base_url: "http://localhost:3000/",
+  // partner_url: "http://localhost:4000/",
+  // base_url: "http://localhost:3000/",
+  partner_url: "https://partner.incity-services.com/",
+  base_url: "https://incity-services.com/",
   getImageLoader: function () {
     return undefined;
     const context = this;
